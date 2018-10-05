7eba48685816fef0cba42ae7f490b29d3e52e1e6
A. Jueling <a.juling@uu.nl>
2018-10-05 14:55:46 +0200

    first exploration of data

---
+++ src/ipynb/copy_stripped_notebooks.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import shutil
+import io
+from nbstripout import strip_output, read, write, NO_CONVERT
+
+destination = '../'
+assert os.path.exists(destination)
+
+# copying stripped .ipynb files
+files = ['data_overview',
+        ]
+for x in files:
+    fn = f'{x}.ipynb'
+    with io.open(fn, 'r', encoding='utf8') as f:
+        nb = read(f, as_version=NO_CONVERT)
+    nb = strip_output(nb, keep_output=False, keep_count=False)
+    with io.open(destination+fn, 'w', encoding='utf8') as f:
+        write(nb, f)
+    print(f'copied {fn}')
